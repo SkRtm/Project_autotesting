91b02331487cf7b50714a54c6cd59aa63438b07c
1 <toymsk488@gmail.com>
2022-02-28 23:10:39 +0300

    Added method which asserts is this login page or not

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .main_page import MainPage
+from .login_page import LoginPage
 
 
 def test_guest_should_go_to_login_link(browser):
@@ -12,3 +13,11 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_guest_should_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)   #browser.current_url - берет адрес, который сейчас есть
+    login_page.should_be_login_page()
