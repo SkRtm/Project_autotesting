e3fd7836232651b7af4d6d81251105083370771f
1 <toymsk488@gmail.com>
2022-02-28 17:27:02 +0300

    Added short review about project and conftest

---
+++ conftest.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+import pytest
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action = 'store', \
+        default = None, help = "Choose browser: chrome or firefox")
+    parser.addoption("--language", action = "store", \
+        default = None, help = "Choose language: ru, en, etc")
+
+
+@pytest.fixture
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    user_language = request.config.getoption("language")
+    if browser_name == "chrome":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        raise pytest.UsageError("--browser name should be chrome or firefox")
+    yield browser
+    browser.quit()
+
+
+
+
