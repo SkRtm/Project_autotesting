58d2c329aa07e19ef9e7e99c82cc764e89420cc4
1 <toymsk488@gmail.com>
2022-02-28 19:21:59 +0300

    Wrong adding of method, now its all right

--- test_main_page.py
+++ test_main_page.py
@@ -11,4 +11,4 @@ def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page()
+    page.should_be_login_link()
