10b8ae3932b2c4828104ba3aee13a69110be5121
1 <toymsk488@gmail.com>
2022-02-28 19:19:55 +0300

    Added wrong locator for test

--- test_main_page.py
+++ test_main_page.py
@@ -6,4 +6,9 @@ def test_guest_should_go_to_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
-    
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
