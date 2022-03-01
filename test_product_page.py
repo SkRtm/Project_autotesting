211da52b40b7122cbc848627bfc5d4e7bd047ef2
1 <toymsk488@gmail.com>
2022-03-01 16:25:00 +0300

    Added test which asserts that success message isnt appeared before adding product to cart

--- test_product_page.py
+++ test_product_page.py
@@ -3,13 +3,13 @@ import pytest
 
 
 def test_guest_should_add_product_to_cart_and_solve_quiz(browser):
-    
     link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_to_cart()
     product_page.solve_quiz_and_get_code()
     product_page.should_be_added_product_to_cart()
+
 
 @pytest.mark.parametrize('parameter', [n if n != 7 else pytest.param(n, marks = pytest.mark.xfail) for n in range(10)])
 def test_find_bug_promo_action(browser, parameter):
@@ -18,4 +18,13 @@ def test_find_bug_promo_action(browser, parameter):
     product_page.open()
     product_page.add_to_cart()
     product_page.solve_quiz_and_get_code()
+    product_page.should_be_added_product_to_cart()
+
+
+def test_success_message_not_presented_before_adding_product_to_cart(browser):
+    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/'
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.shouldnt_be_success_message()
+    product_page.add_to_cart()
     product_page.should_be_added_product_to_cart()
