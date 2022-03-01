883967a2e97987ad58161cb138ddfe4a9c10e56f
1 <toymsk488@gmail.com>
2022-03-01 16:04:46 +0300

    Added method, that asserts success message isn	 presented before adding product to cart

--- product_page.py
+++ product_page.py
@@ -5,7 +5,7 @@ from selenium.common.exceptions import NoAlertPresentException
 
 class ProductPage(BasePage):
 
-    def solve_quiz_and_get_code(self):
+    def solve_quiz_and_get_code(self): #решает уравнение из окошка alert
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
         answer = str(log(abs((12 * sin(float(x))))))
@@ -19,10 +19,12 @@ class ProductPage(BasePage):
         except NoAlertPresentException:
             print("No second alert presented")
 
-    def add_to_cart(self):
+    def add_to_cart(self): 
+        #добавляет товар в корзину
         self.browser.find_element(*ProductPageLocators.ADD_TO_CART_BUTTON).click()
 
-    def should_be_the_same_name_of_product_added_to_cart_and_product(self):
+    def should_be_the_same_name_of_product_added_to_cart_and_product(self): \
+        #сравнивает имя товара и имя товара в корзине
         product_name = self.browser.\
             find_element(*ProductPageLocators.NAME_OF_PRODUCT).text
         product_name_added_to_cart = self.browser.\
@@ -31,6 +33,7 @@ class ProductPage(BasePage):
             f"Names of products are not the same. Expected {product_name} instead of {product_name_added_to_cart}"
     
     def should_be_the_same_price_of_cart_and_product(self):
+        #сравнивает цену товара и цену товара в корзине
         product_price = self.browser.\
             find_element(*ProductPageLocators.PRICE_OF_PRODUCT).text
         price_of_cart = self.browser.\
@@ -39,5 +42,11 @@ class ProductPage(BasePage):
             f"Prices of products are not the same. Expected {product_price} instead of {price_of_cart}"
 
     def should_be_added_product_to_cart(self):
+        #объединение функций на сравнение цен и имен товара в одну
+        #если какая-лбо из функций не пройдет assert, выполнение этой функции прекратится
         self.should_be_the_same_name_of_product_added_to_cart_and_product()
         self.should_be_the_same_price_of_cart_and_product()
+
+    def shouldnt_be_success_message(self):
+        assert self.is_not_element_presented(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented before product added to cart"
