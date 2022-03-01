8f3b6ec61764b6a725d8111be50f1f557c9b1de2
1 <toymsk488@gmail.com>
2022-03-01 16:16:25 +0300

    Added expected_condition for is_not_element_presented method

--- base_page.py
+++ base_page.py
@@ -1,11 +1,13 @@
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
 
 class BasePage:
 
-    def __init__(self, browser, url, timeout = 10):
+    def __init__(self, browser, url):
         self.browser = browser
         self.url = url
-        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
@@ -15,6 +17,15 @@ class BasePage:
             self.browser.find_element(method, locator)
         
         except NoSuchElementException:
+            return False
+
+        return True
+
+    def is_not_element_presented(self, method, locator, timeout = 4):
+        try:
+            WebDriverWait(self.browser, timeout).\
+                until(EC.presence_of_element_located((method, locator)))
+        except TimeoutException:
             return False
 
         return True
