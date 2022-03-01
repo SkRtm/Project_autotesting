a98d9c7c4d20d5887bb7b00bcc3738a7fd4749e8
1 <toymsk488@gmail.com>
2022-03-01 15:26:36 +0300

    Modified parametrize for links with list comprehension

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,7 @@
 from .product_page import ProductPage
 import pytest
 
+numbers = [i for i in range(10)] #generates numbers from 0 to 9, which are in link after promo=offer
 
 def test_guest_should_add_product_to_cart_and_solve_quiz(browser):
     
@@ -11,17 +12,9 @@ def test_guest_should_add_product_to_cart_and_solve_quiz(browser):
     product_page.solve_quiz_and_get_code()
     product_page.should_be_added_product_to_cart()
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_find_bug_promo_action(browser, link):
+@pytest.mark.parametrize('parameter', numbers)
+def test_find_bug_promo_action(browser, parameter):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{parameter}"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_to_cart()
