f63e29231db97cf08fe4a74e8723346b8232566c
1 <toymsk488@gmail.com>
2022-03-01 15:12:50 +0300

    Added test with parametrized links for promo action

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,27 @@
 from .product_page import ProductPage
+import pytest
 
 
 def test_guest_should_add_product_to_cart_and_solve_quiz(browser):
     
     link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_to_cart()
+    product_page.solve_quiz_and_get_code()
+    product_page.should_be_added_product_to_cart()
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_find_bug_promo_action(browser, link):
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_to_cart()
