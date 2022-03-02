65992f7c4c2a2a9efdeeab6bdb994bcc444c9617
1 <toymsk488@gmail.com>
2022-03-02 16:35:25 +0300

    Grouped tests with login in one class and marked it like login_guest

--- test_main_page.py
+++ test_main_page.py
@@ -1,19 +1,21 @@
 from .base_page import BasePage
 from .login_page import LoginPage
 from .basket_page import BasketPage
+import pytest
 
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
+    def test_guest_should_go_to_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = BasePage(browser, link)
+        page.open()
+        page.go_to_login_page()
 
-def test_guest_should_go_to_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = BasePage(browser, link)
-    page.open()
-    page.go_to_login_page()
-
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = BasePage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = BasePage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 def test_guest_should_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
