02c619323390e3467c00d41a4febb9e663e55397
1 <toymsk488@gmail.com>
2022-03-02 15:27:05 +0300

    Now methods from main_page in base_page

--- base_page.py
+++ base_page.py
@@ -2,6 +2,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
+from .locators import BasePageLocators
 
 class BasePage:
 
@@ -40,3 +41,12 @@ class BasePage:
             return False
         
         return True
+
+    
+    def go_to_login_page(self):
+        self.browser.find_element(*BasePageLocators.LOGIN_LINK).click()
+    
+
+    def should_be_login_link(self):
+        assert self.browser.find_element(*BasePageLocators.LOGIN_LINK),\
+            "Login link isnt presented here"
