18171c4fd08515a80fe4b2dca42f4d0d3842f8cf
1 <toymsk488@gmail.com>
2022-03-02 15:29:28 +0300

    Rewrited methods for tests from MainPage(arg1, arg2) to BasePage(arg1, arg2), cuz of moving methods from main_page to base_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,22 +1,22 @@
-from .main_page import MainPage
+from .base_page import BasePage
 from .login_page import LoginPage
 
 
 def test_guest_should_go_to_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    page = BasePage(browser, link)
     page.open()
     page.go_to_login_page()
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    page = BasePage(browser, link)
     page.open()
     page.should_be_login_link()
 
 def test_guest_should_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
-    page = MainPage(browser, link)
+    page = BasePage(browser, link)
     page.open()
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)   #browser.current_url - берет адрес, который сейчас есть
